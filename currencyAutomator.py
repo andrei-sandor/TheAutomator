dd59a2bf4f65189a553ad29f61238c92cf5b2d9e
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2024-06-02 13:47:35 -0400

    Done currency automator

---
+++ currencyAutomator.py
@@ -0,0 +1,22 @@
+from forex_python.converter import CurrencyRates
+from forex_python.bitcoin import BtcConverter
+import seaborn
+import datetime import date
+
+def currencies_automator(in_currency, out_currency):
+    c = CurrencyRates()
+    result = c.convert(str(in_currency), str(out_currency))
+    return result
+
+def get_bitcoins(in_currency):
+    b = BtcConverter()
+    result = b.get_latest_price(str(in_currency))
+    return result
+
+def get_conversion_rates(in_currency):
+    c = CurrencyRates()
+    dict_currency_rates_today = c.get_rates(str(in_currency), date.today())
+    seaborn.scatterplot(dict_currency_rates_today,x="Currencies", y=("Value in" , str(in_currency)))
+
+
+
