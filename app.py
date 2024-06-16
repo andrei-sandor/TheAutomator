270ef1199cbb0cf411fc824edb853582a80483d4
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2024-06-16 10:35:57 -0400

    Done UI

--- app.py
+++ app.py
@@ -11,35 +11,12 @@ from currencyAutomator import *
 app = Flask(__name__)
 app.debug = True
 
-# adding configuration for using a sqlite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-# Creating an SQLAlchemy instance
 db = SQLAlchemy(app)
-
-# Settings for migrations
 migrate = Migrate(app, db)
 
 
-# Models
-class Profile(db.Model):
-    # Id : Field which stores unique id for every row in
-    # database table.
-    # first_name: Used to store the first name if the user
-    # last_name: Used to store last name of the user
-    # Age: Used to store the age of the user
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(20), unique=False, nullable=False)
-    last_name = db.Column(db.String(20), unique=False, nullable=False)
-    age = db.Column(db.Integer, nullable=False)
 
-    # repr method represents how one object of this datatable
-    # will look like
-    def __repr__(self):
-        return f"Name : {self.first_name}, Age: {self.age}"
-
-
-# function to render index page
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -153,16 +130,23 @@ def profile():
     else:
         return redirect('/')
 
-
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
 @app.route('/delete/<int:id>')
 def erase(id):
-    # deletes the data on the basis of unique id and
-    # directs to home page
     data = Profile.query.get(id)
     db.session.delete(data)
     db.session.commit()
     return redirect('/')
 
+
+
+
+    def __repr__(self):
+        return f"Name : {self.first_name}, Age: {self.age}"
 
 if __name__ == '__main__':
     app.run()
